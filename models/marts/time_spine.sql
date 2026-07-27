d86b74dce8c3f5b638832903d19d638796f5ae58
lydzindata@gmail.com <lydzindata@gmail.com>
2026-07-27 14:36:07 +0000

    commit  all changed-- models, marts, seed

---
+++ models/marts/time_spine.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+select
+    dateadd(
+        day,
+        seq4(),
+        '2020-01-01'::date
+    ) as date_day
+
+from table(
+    generator(
+        rowcount => 3653
+    )
+)
